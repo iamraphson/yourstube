e26dd16e8dd3fa0faebfa88dcd4f46e6f3e10c76
Ayeni Olusegun <nsegun5@gmail.com>
2016-06-28 01:58:10 +0100

    refactor(yourstube): make some changes in the route file

--- server/routes.js
+++ server/routes.js
@@ -2,13 +2,14 @@
  * Created by Raphson on 6/27/16.
  */
 var userCtrl = require('./controllers/user.server.controller');
+var token = require('../config/token');
 
 module.exports = function(app){
-    app.get('/api', userCtrl.welcome);
+    app.get('/api', token.ensureAuthenticated, userCtrl.welcome);
 
     app.post('/api/login', userCtrl.auth);
     app.post('/api/register', userCtrl.registerUser);
 
-    app.put('/api/me', userCtrl.updateLoggedInUserDetail);
-    app.get('/api/me', userCtrl.getLoggedInUserDetail)
+    app.put('/api/me', token.ensureAuthenticated, userCtrl.updateLoggedInUserDetail);
+    app.get('/api/me', token.ensureAuthenticated, userCtrl.getLoggedInUserDetail)
 };
