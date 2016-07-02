ff7d05be4e538f4216584d024205300acda390ad
Ayeni Olusegun <nsegun5@gmail.com>
2016-07-02 23:06:26 +0100

    trying to implement upload functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,21 @@
 /**
  * Created by Raphson on 6/28/16.
  */
-var app = angular.module('yourstube', ['ngCookies', 'ngRoute', 'ngStorage', 'ngMessages', 'angularMoment', 'angular-loading-bar',
-    'ui.bootstrap', 'toastr', 'ngSanitize', 'ngLodash', 'satellizer', 'angularUtils.directives.dirDisqus', 'appRoutes'])
+var app = angular.module('yourstube', ['ngCookies', 'ngRoute', 'ngStorage', 'ngMessages', 'angularMoment', 'cloudinary','angular-loading-bar',
+    'ui.bootstrap', 'toastr', 'ngFileUpload', 'ngSanitize', 'ngLodash', 'satellizer',
+    'angularUtils.directives.dirDisqus', 'appRoutes'])
     .config(['cfpLoadingBarProvider', '$httpProvider','$authProvider', function(cfpLoadingBarProvider, $httpProvider, $authProvider){
 
         $authProvider.baseUrl = '/';
         $authProvider.loginUrl = '/api/login';
         $authProvider.signupUrl = '/api/register';
-        $authProvider.authHeader = 'Authorization';
+        $authProvider.authHeader = 'Authorization';             
         $authProvider.authToken = 'Bearer';
         $authProvider.storageType = 'localStorage';
 
 
         cfpLoadingBarProvider.includeBar = true;
         cfpLoadingBarProvider.includeSpinner = false;
+
+        //cloudinaryProvider.set("cloud_name", "unicodeveloper").set("upload_preset", "b9ej8dr5");
     }]);
--- public/js/controller/upload.client.controller.js
+++ public/js/controller/upload.client.controller.js
@@ -1,6 +1,47 @@
 /**
  * Created by Raphson on 7/1/16.
  */
-app.controller('UploadController', function($scope){
+app.controller('UploadController', function($scope, $rootScope, $location, Upload, $http){
 
+    $scope.uploadFiles = function(files){
+        $scope.files = files;
+        if (!$scope.files) return;
+        angular.forEach(files, function(file){
+            if (file && !file.$error) {
+
+                file.upload = Upload.upload({
+                    url: "/api/upload",
+                    method: "POST",
+                    data: {
+                        file: file
+                    }
+                }).progress(function (e) {
+                    file.status = "Uploading...";
+                    file.progress = Math.round((e.loaded * 100.0) / e.total);
+                }).success(function (data, status, headers, config) {
+                    file.status = "Done...100%";
+                    file.result = data;
+                }).error(function (data, status, headers, config) {
+                    file.result = data;
+                });
+            }
+        });
+    };
+
+    /* Modify the look and fill of the dropzone when files are being dragged over it */
+    $scope.dragOverClass = function($event) {
+        var items = $event.dataTransfer.items;
+        var hasFile = false;
+        if (items !== null) {
+            for (var i = 0 ; i < items.length; i++) {
+                if (items[i].kind == 'file') {
+                    hasFile = true;
+                    break;
+                }
+            }
+        } else {
+            hasFile = true;
+        }
+        return hasFile ? "dragover" : "dragover-err";
+    };
 });
